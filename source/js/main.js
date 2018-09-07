822db7fd873e4dc123c077d0f36cb8af3dc74e6f
Timur Adolshin <tadolshin@gmail.com>
2018-09-07 22:25:42 +0300

    preloader

--- source/js/main.js
+++ source/js/main.js
@@ -23,16 +23,29 @@ myFunction();
 $(window).resize(function () {
   myFunction();
 });
-//Вызов поиска на таблетке
-// var inp = $('.header__input')
-// if (!inp.value) {
-//   $('.header__input-submit_click').click(function (e) {
-//     e.preventDefault();
-//     $('.header__input').toggleClass('header__input_active');  
-//   });
-// }
+//-Прелоадер
+var
+    images = document.images,
+    images_total_count = images.length,
+    images_loaded_count = 0;
+perc_display = document.getElementById('count');
 
-// $('.header__input-cross').click(function (e) {
-//   e.preventDefault();
-//   $('.header__input').removeClass('header__input_active'); 
-// });
+for (var i = 0; i < images_total_count; i++) {
+    image_clone = new Image();
+    image_clone.onload = image_loaded;
+    image_clone.onerror = image_loaded;
+    image_clone.src = images[i].src;
+}
+// console.log(images_total_count);
+// console.log(images_loaded_count);
+function image_loaded() {
+    images_loaded_count++;
+    perc_display.innerHTML = (((100 / images_total_count) * images_loaded_count) << 0) + '%';
+
+    if (images_loaded_count >= images_total_count) {
+        $(window).on('load', function () {
+            $('.wrapper').removeClass('visuallyhidden');
+            $('.preloader').delay(2000).fadeOut(2000);
+        });
+    }
+};
